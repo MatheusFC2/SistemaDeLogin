ae971fdca4fbcb568608fd5d30618aa05308ce08
MatheusFC2 <matheusfreitascampos@gmail.com>
2022-06-19 19:09:10 -0300

    feat: imported Routes and GlobalStyle

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
-import './App.css'
+import RoutesApp from './routes'
 import GlobalStyle from './styles/global'
 
 function App() {
 
   return (
     <>
-    <div>alksjdh</div>
-    <GlobalStyle/> 
+      <RoutesApp/>
+      <GlobalStyle/> 
     </>
   )
 }
