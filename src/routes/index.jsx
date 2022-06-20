0193ea1cbd094b4f259ed6b26eee7692ee63f9e0
MatheusFC2 <matheusfreitascampos@gmail.com>
2022-06-20 11:30:56 -0300

    chore: home to Home

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -17,7 +17,7 @@ const RoutesApp = () => {
         <BrowserRouter>
             <Fragment>
                 <Routes>
-                    <Route exact path="/home" element={<Private Item={Home} />} />
+                    <Route exact path="/Home" element={<Private Item={Home} />} />
                     <Route path="/" element={<Signin />} />
                     <Route exact path="/signup" element={<Signup />} />
                     <Route path="*" element={<Signup />} />
